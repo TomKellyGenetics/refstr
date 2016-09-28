72244085120d0227e83e892ec7bb1c598134f1c9
TomKellyGenetics <tom.kelly@postgrad.otago.ac.nz>
2016-09-28 16:41:24 +1300

    bash data prep

---
+++ refstr.sh
@@ -0,0 +1,9 @@
+#! \bin\bash
+# args: input_vcf ref_seq output_name
+
+#tail removes header, sed removes lines breaks
+tail -n +2 $2 | sed ':a;N;$!ba;s/\n/ /g' > reference_string.txt
+
+#remove header from VCF
+grep "#CHROM" output1.vcf > output.txt
+sed -n -E -e '/#CHROM/,$ p' output1.vcf| sed '1 d' >> output.txt
