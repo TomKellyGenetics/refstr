9695e57ff8eb1a262968ef37ffdbfbbe339ef828
TomKellyGenetics <tom.kelly@postgrad.otago.ac.nz>
2016-09-28 16:53:09 +1300

    reference sequence in R

---
+++ refstr.R
@@ -0,0 +1,10 @@
+#load inpute data
+reference <- readLines("reference_string.txt")
+vcf_cols <- read.table("output.txt")
+
+#split reference string into character vector
+reference <- strsplit(reference, split="")[[1]]
+#positions of reference
+names(reference) <- 1:length(reference)
+
+
--- refstr.sh
+++ refstr.sh
@@ -2,7 +2,7 @@
 # args: input_vcf ref_seq output_name
 
 #tail removes header, sed removes lines breaks
-tail -n +2 $2 | sed ':a;N;$!ba;s/\n/ /g' > reference_string.txt
+tail -n +2 $2 | sed ':a;N;$!ba;s/\n//g' > reference_string.txt
 
 #remove header from VCF
 grep "#CHROM" output1.vcf > output.txt
