b2dbfcd58d7560bbd29c136ded495a9f9168713d
TomKellyGenetics <tom.kelly@postgrad.otago.ac.nz>
2016-09-28 20:18:02 +1300

    enables passing input arguments to R and variable intermediary filenames

--- refstr.R
+++ refstr.R
@@ -1,7 +1,7 @@
 #load input data
 args = commandArgs(trailingOnly=TRUE)
-reference <- readLines("reference_string.txt")
-vcf_cols <- data.table::fread("output.tsv", data.table = F)
+reference <- readLines(as.character(args[6]), warn=F)
+vcf_cols <- data.table::fread(as.character(args[5]), data.table = F)
 
 #split reference string into character vector
 reference <- strsplit(reference, split="")[[1]]
@@ -12,19 +12,20 @@ names(reference) <- 1:length(reference)
 length_reference <- nchar(vcf_cols$REF)
 length_alternate <- nchar(vcf_cols$ALT)
 #account for missing points as "."
-length_reference[grep("[.]", vcf_cols$REF)] <- 0
+#length_reference[grep("[.]", vcf_cols$REF)] <- 0
 length_alternate[grep("[.]", vcf_cols$ALT)] <- 0
 
 frameshifts <- length_alternate - length_reference
-table(frameshifts) # do we want to provide a text summary of frameshits detected?
+#table(frameshifts) # do we want to provide a text summary of frameshits detected?
 # % subs + threshold of frameshifts could be warnings for new assembly / variant calling
 
 #initialise new sequence
 new_sequence <- reference
 #substitute new variants into fasta sequence
 for(ii in 1:nrow(vcf_cols)){
-  new_sequence[vcf_cols$POS[ii]:(vcf_cols$POS[ii]+length_reference[ii]-1)] <- vcf_cols$ALT[ii]
-  print(paste(reference[vcf_cols$POS[ii]:(vcf_cols$POS[ii]+length_reference[ii]-1)], "substituted for", vcf_cols$ALT[ii]))
+  new_sequence[vcf_cols$POS[ii]] <- vcf_cols$ALT[ii]
+  if(length_reference>1) new_sequence[(vcf_cols$POS[ii]+1):(vcf_cols$POS[ii]+length_reference[ii]-1)] <- NA
+  print(paste(paste(reference[vcf_cols$POS[ii]:(vcf_cols$POS[ii]+length_reference[ii]-1)], collapse=""), "substituted for", vcf_cols$ALT[ii]))
 }
 
 #test output
