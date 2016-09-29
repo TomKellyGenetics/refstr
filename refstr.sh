c61bbef07e45c9826909286b5c29529912a9119b
TomKellyGenetics <tom.kelly@postgrad.otago.ac.nz>
2016-09-30 12:35:18 +1300

    switch to julia version

--- refstr.sh
+++ refstr.sh
@@ -23,8 +23,6 @@ grep "#CHROM" $1 > $input_tsv
 sed -n -E -e '/#CHROM/,$ p' $1 | sed '1 d' >> $input_tsv
 sed -i 's/#//g' $input_tsv
 
-#Rscript refstr.R "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
-#python refstr.py "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
 ./refstr.jl "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
 
 #cut fasta to rows of 60 chars
