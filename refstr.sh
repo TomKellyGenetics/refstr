204d3841abe44cee7c3ad673ab8784e22b1cbf0e
TomKellyGenetics <tom.kelly@postgrad.otago.ac.nz>
2016-09-30 12:45:07 +1300

    consolidate R version

--- refstr.sh
+++ refstr.sh
@@ -23,9 +23,7 @@ grep "#CHROM" $1 > $input_tsv
 sed -n -E -e '/#CHROM/,$ p' $1 | sed '1 d' >> $input_tsv
 sed -i 's/#//g' $input_tsv
 
-#Rscript refstr.R "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
-#python refstr.py "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
-./refstr.jl "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
+Rscript refstr.vanilla.R "${@:1:5}" $input_tsv $reference_string ${6:-1} #initialise R script and pass pass arguments
 
 #cut fasta to rows of 60 chars
 fold -w 60 -s ${4}_string.txt > ${4}.fasta
