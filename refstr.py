5f91f417e01ddeb3612644c2e8a52475c1db687f
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-10-18 18:35:58 +0900

    gtf python

--- refstr.py
+++ refstr.py
@@ -10,6 +10,7 @@ reference = open(args[7], 'r').readline()
 import numpy as np
 import pandas as pd
 vcf_cols = pd.read_csv(args[6], sep='\t', header=0)
+vcf_cols = vcf_cols.sort()
 
 #split reference string into character vector
 reference = list(reference)
@@ -34,11 +35,11 @@ for row in range(len(vcf_cols.index)):
 new_sequence = reference
 #substitute new variants into fasta sequence
 for ii in range(len(vcf_cols.index)):
-    new_sequence[vcf_cols.loc[ii,'POS']-1] = vcf_cols.loc[ii,'ALT']
+    new_sequence[(vcf_cols.loc[ii,'POS']-1)] = vcf_cols.loc[ii,'ALT']
     if length_reference[ii] > 2:
-        new_sequence[vcf_cols.loc[ii,'POS']:(vcf_cols.loc[ii,'POS']+length_reference[ii]-1)] = "NA"
+        new_sequence[vcf_cols.loc[ii,'POS']:(vcf_cols.loc[ii,'POS']+length_reference[ii]-1)] = None
     elif length_reference[ii] > 1:
-        new_sequence[vcf_cols.loc[ii,'POS']] = "NA"
+        new_sequence[vcf_cols.loc[ii,'POS']] = None
     print vcf_cols.loc[ii,'REF'],"substituted for",vcf_cols.loc[ii,'ALT']
 
 #test output
@@ -49,14 +50,16 @@ for ii in range(len(vcf_cols.index)):
 #length(na.omit(new_sequence))
 #table(is.na(new_sequence))
 
-#remove empty spaces
-new = []
-for seq in new_sequence:
-    if seq != "NA":
-        new.append(seq)
-new_sequence = new
+
 #concatenate into one string
 new_sequence = ''.join(new_sequence)
+#remove empty spaces
+new_sequence.replace("n", "")
+#new = []
+#for seq in new_sequence:
+#    if seq != "None":
+#        new.append(seq)
+#new_sequence = new
 
 #test fasta
 len(new_sequence)
@@ -65,11 +68,11 @@ len(new_sequence)
 open(''.join([args[4], "_string.txt"]), 'w+').write(new_sequence)
 
 #read gtf data
-#gtf <- data.table::fread(as.character(args[3]), data.table = F)
+gtf = pd.read_csv(args[3], sep='\t', header=0)
 
 #calculate frameshifts
-#frameshifts <- length_alternate - length_reference
-#adj_gtf_pos <- cumsum(frameshifts[order(vcf_cols.loc[:,'POS'])])
+frameshifts = np.array(length_alternate) - np.array(length_reference)
+adj_gtf_pos = frameshifts.cumsum()
 #table(frameshifts) # do we want to provide a text summary of frameshits detected?
 # % subs + threshold of frameshifts could be warnings for new assembly / variant calling
 
@@ -78,29 +81,50 @@ open(''.join([args[4], "_string.txt"]), 'w+').write(new_sequence)
 #vcf_cols.loc[:,'POS']
 
 #add starting point to new positions
-#if(vcf_cols$POS[1]!=1){
-#    position <- c(1, vcf_cols.loc[:,'POS'][order(vcf_cols.loc[:,'POS'])])
-#    adj_gtf_pos <- c(0, adj_gtf_pos)
-#  } else {
-#   position <- vcf_cols.loc[:,'POS'][order(vcf_cols$POS)]
-#}
-#if(position[length(position)]!=length(reference)){
-#  position <- c(position, length(reference))
-#} 
+position = vcf_cols.loc[:,'POS']
+if vcf_cols[:POS][1] != 1:
+    position = pd.Series([1])
+    position.append(vcf_cols.loc[:,'POS'])
+    temp = [1]
+    temp.extend(adj_gtf_pos)
+    adj_gtf_pos = temp
+    temp = [1]
+    temp.extend(frameshifts)
+    frameshifts = temp
+position = position.append(pd.Series(len(reference)))
 
 
 #new pos
-#new_positions <- as.numeric(original_position)
-#for(ii in 1:(length(position)-1)){
-#  new_positions[position[ii]:(position[ii+1]-1)] <-new_positions[position[ii]:(position[ii+1]-1)] + adj_gtf_pos[ii]
-#}
-#new_positions[length(new_positions)] <- len(new_sequence)
+#new_positions = []
+#for jj in range(len(position)-1):
+#    if frameshifts[jj] < 0:
+#        for i in range(1,-frameshifts[jj]+1)
+#            new_positions.append(position[jj]+adj_gtf_pos[jj-1]) #repeats for del
+#        new_positions.append(range(-frameshifts[jj]+position[jj]+adj_gtf_pos[jj]),(position[jj+1]adj_gtf_pos[jj])))
+#    if frameshifts[jj] > 0:
+#        new_positions.append(position[jj]+adj_gtf_pos[jj]) #skip ins
+#        new_positions.append(range(frameshifts[jj]+position[jj]+adj_gtf_pos[jj-1]+1),(position[jj+1]+adj_gtf_pos[jj]))) #skip ins
+#    if frameshifts[jj] == 0:
+#        new_positions.append(range(position[jj]+adj_gtf_pos[jj]),(position[jj+1]+adj_gtf_pos[jj]))) #keep pos for subs
+#    print(jj)
+#
+
+
+#new_positions = original_position
+#for ii in range(len(position)-1):
+
+
+new_positions = []
+for ii in range(len(position)-1):
+    new_positions.append(map(lambda x: x+adj_gtf_pos[ii],  original_position[position[ii]:(position[ii+1]-1)]))
+new_positions.append(range(position[len(position)-1], len(new_sequence)+1)+adj_gtf_pos[len(position)-1])
+new_positions = [item for sublist in new_positions for item in sublist]
 
 #substitute new gtf positions #do we want to warn if frameshift within a gene?
-#new_gtf <- gtf
-#new_gtf$V4 <- new_positions[gtf$V4]
-#new_gtf$V5 <- new_positions[gtf$V5]
+new_gtf = gtf
+new_gtf[:, 3] = new_positions[gtf[:, 3]]
+new_gtf[:, 4] = new_positions[gtf[:, 4]]
 
 #output gtf file
-#write.table(new_gtf, file = paste0(args[4], ".gtf"), header=F, quotes=F)
+write.table(new_gtf, file = paste0(args[4], ".gtf"), header=F, quotes=F)
 
